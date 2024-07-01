98fae9751c577e665394e7175eff99c877e5b831
Jhosen Alcon <jhosen.alcon@gmail.com>
2024-07-01 18:01:02 +0200

    cambio de menu, galeria y creacion de videooss

---
+++ Exhibiz HTML/video-list.js
@@ -0,0 +1,22 @@
+let allVideos = [
+   {
+      name: "3 Speakers Internacionales",
+      src: "CROOKER",
+      id: "vid_1"
+   },
+   {
+      name: "3 Speakers Internacionales",
+      src: "3 SPEAKERS HORIZONTAL",
+      id: "vid_2"
+   },
+   {
+      name: "Invitación de Alejandra",
+      src: "INVITACION ALEJANDRA comprimido(1)",
+      id: "vid_3"
+   },
+   {
+      name: "CROOKER",
+      src: "CROOKER",
+      id: "vid_4"
+   }
+]
